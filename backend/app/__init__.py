1f91d12ffcacedbc577a22f4b8728f372fe42ea4
Timofey <timabd77@gmail.com>
2023-12-03 14:23:54 +0300

    fixing CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 # создание экземпляра приложения
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:8082"])
 
 # инициализирует расширения
 
