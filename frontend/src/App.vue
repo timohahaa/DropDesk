5989649e2917baf7e2b6b3278fe22c1559fac86e
Khush <ganievkhushbaht@gmail.com>
2023-12-03 19:06:44 +0300

    feta: add res style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,8 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
 </template>
 
-<style lang="scss">
+<style lang="scss" src="@/assets/scss/resetstyle.scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
